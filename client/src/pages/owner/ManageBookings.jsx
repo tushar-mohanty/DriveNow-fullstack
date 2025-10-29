16b7eb4185bb4d7f7eeef970458c098aa869958e
Tushar Mohanty <tusharmohanty100@gmail.com>
2025-10-30 01:05:17 +0530

    Update ManageBookings.jsx

--- client/src/pages/owner/ManageBookings.jsx
+++ client/src/pages/owner/ManageBookings.jsx
@@ -20,7 +20,7 @@ const ManageBookings = () => {
 
   const changeBookingStatus = async(bookingId, status)=>{
     try {
-      const { data } = await axios.post('/api/bookings/change-status')
+      const { data } = await axios.post('/api/bookings/change-status', {bookingId, status})
       if(data.success){
         toast.success(data.message)
         fetchOwnerBookings()
